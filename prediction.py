b0be5d1d95112f4c13c361cbcd079c2f974d82bd
Fynaritra <finaritra.mickaelle@gmail.com>
2023-08-03 23:32:03 +0400

    Appeler model pkl genere par Jupyter

---
+++ prediction.py
@@ -0,0 +1,48 @@
+import joblib
+import numpy as np
+import pandas as pd
+import pickle
+
+# Load the model from the file
+model = joblib.load('model_2.pkl')
+le = joblib.load('label_encoder.pkl')
+
+# Define a sample observation with 13 features (replace this with your actual data)
+feature_names = [
+    "HOURS_DATASCIENCE", "HOURS_BACKEND", "HOURS_FRONTEND",
+    "NUM_COURSES_BEGINNER_DATASCIENCE", "NUM_COURSES_BEGINNER_BACKEND",
+    "NUM_COURSES_BEGINNER_FRONTEND", "NUM_COURSES_ADVANCED_DATASCIENCE",
+    "NUM_COURSES_ADVANCED_BACKEND", "NUM_COURSES_ADVANCED_FRONTEND",
+    "AVG_SCORE_DATASCIENCE", "AVG_SCORE_BACKEND", "AVG_SCORE_FRONTEND"
+]
+
+sample_data = [
+    28, # HOURS_DATASCIENCE
+    7,       # HOURS_BACKEND
+    39,      # HOURS_FRONTEND
+    29,      # NUM_COURSES_BEGINNER_DATASCIENCE
+    2,       # NUM_COURSES_BEGINNER_BACKEND
+    4,       # NUM_COURSES_BEGINNER_FRONTEND
+    0,       # NUM_COURSES_ADVANCED_DATASCIENCE
+    2,       # NUM_COURSES_ADVANCED_BACKEND
+    5,       # NUM_COURSES_ADVANCED_FRONTEND
+    84,      # AVG_SCORE_DATASCIENCE
+    74,      # AVG_SCORE_BACKEND
+    0        # AVG_SCORE_FRONTEND
+]
+
+# Make sure the input data is in the correct shape (1, number_of_features)
+# input_data = np.array(sample_data).reshape(1, -1)
+
+# Use the model to predict the target
+# prediction = model.predict(input_data)
+
+# Create a DataFrame with the correct feature names
+input_data_df = pd.DataFrame([sample_data], columns=feature_names)
+
+# Predict using the DataFrame
+prediction = model.predict(input_data_df)
+data = le.inverse_transform(prediction)
+
+# Print the prediction
+print("Prediction:", data)

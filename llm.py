182398e387c16e9de79bd4fae618ff94439bad0a
Jexblue <91774746+Jexblue@users.noreply.github.com>
2023-09-09 17:28:13 +0300

    Scrap PDF online

--- llm.py
+++ llm.py
@@ -10,18 +10,28 @@ from langchain.llms import OpenAI
 from langchain import PromptTemplate
 import openai
 
+import scrapUrl as su
+import scrapPdf as sp
+
 # URL of the Wikipedia page to scrape
-url = 'https://en.wikipedia.org/wiki/Ranavalona_I'
+url = 'https://towardsdatascience.com/all-you-need-to-know-to-build-your-first-llm-app-eb982c78ffac'
 
 # Send a GET request to the URL
-response = requests.get(url)
+# response = requests.get(url)
 
 # Parse the HTML content using BeautifulSoup
-soup = BeautifulSoup(response.content, 'html.parser')
+# soup = BeautifulSoup(response.content, 'html.parser')
 
 # Find all the text on the page
-text = soup.get_text()
-text = text.replace('\n', '')
+# text = soup.get_text()
+# scrapping text from website
+scrapU = su.scrapUrl("https://en.wikipedia.org/wiki/GPT-4")
+
+# scapping text from PDF
+# scrapP = sp.load_pdf_content("C:/Users/JerryHeritiana(RAPP)/OneDrive - OneWorkplace/Documents/IAGORA/FUNCHATGPTSerge.pdf")
+scrapP = sp.load_pdf_content("https://www.furet.com/media/pdf/feuilletage/9/7/8/2/8/0/4/1/9782804171018.pdf")
+
+text = scrapP.replace('\n', '')
 
 # Open a new file called 'output.txt' in write mode and store the file object in a variable
 with open('output.txt', 'w', encoding='utf-8') as file:
@@ -47,7 +57,7 @@ embeddings = OpenAIEmbeddings()
 # use the text chunks and the embeddings model to fill our vector store
 db = Chroma.from_documents(texts, embeddings)
 
-user_question = "Parle moi de Ranavalona I"
+user_question = "C'est quoi la transmission de l'information genetique"
 
 # use our vector store to find similar text chunks
 results = db.similarity_search(
--- scrapPdf.py
+++ scrapPdf.py
@@ -1,32 +1,68 @@
+# import PyPDF2
+
+# def load_pdf_content(filepath):
+
+#   with open(filepath, 'rb') as f:
+#     pdf = PyPDF2.PdfReader(f)
+#     text = ''
+#     for page in pdf.pages:
+#       text += page.extract_text()
+
+#   return text
+
 import PyPDF2
+import requests
+from io import BytesIO
 
-def load_pdf_content(filepath):
+def load_pdf_content(url):
+    response = requests.get(url)
+    response.raise_for_status()  # Raise an error for bad responses
 
-  with open(filepath, 'rb') as f:
-    pdf = PyPDF2.PdfReader(f)
-    text = ''
-    for page in pdf.pages:
-      text += page.extract_text()
+    with BytesIO(response.content) as f:
+        pdf = PyPDF2.PdfReader(f)
+        text = ''
+        for page in pdf.pages:
+            text += page.extract_text()
 
-  return text
+    return text
 
 
-#import requests
-#import PyPDF2
-#import os
+# import PyPDF2
+# import requests
+# import chardet
 
-#def load_pdf_content(url):
+# def remove_null_characters(pdf_content):
 
- # response = requests.get(url)
+#   return pdf_content.replace('\0', '')
 
-  #with open("temp.pdf", "wb") as f:
-   # f.write(response.content)
+# def load_pdf_content(filepath):
 
-  #pdf = PyPDF2.PdfReader("temp.pdf")
-  #text = ''
-  #for page in pdf.pages:
-   # text += page.extract_text()
+#   with open(filepath, 'rb') as f:
+#     pdf = PyPDF2.PdfReader(f)
+#     text = ''
+#     for page in pdf.pages:
+#       text += page.extract_text()
 
-  #os.remove("temp.pdf")
+#   return text
 
-  #return text
+# def load_online_pdf_content(url):
+
+#   response = requests.get(url)
+#   pdf_content = response.content
+
+#   # Detect the encoding of the PDF file.
+#   encoding = chardet.detect(pdf_content)['encoding']
+
+#   # If the encoding is None, use iso-8859-1 as default.
+#   if encoding is None:
+#     encoding = 'iso-8859-1'
+
+#   # Decode the PDF file using the detected encoding.
+#   pdf_content = pdf_content.decode(encoding)
+
+#   # Remove null characters from the PDF file.
+#   pdf_content = remove_null_characters(pdf_content)
+
+#   return load_pdf_content(pdf_content)
+
+
